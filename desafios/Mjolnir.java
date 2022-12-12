f5670d36c0376eeb2efebdeb11a6c4504434a6e7
Minni <lucasminni@hotmail.com>
2022-12-11 22:51:52 -0300

    Desafio

---
+++ desafios/Mjolnir.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Mjolnir {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int C = leitor.nextInt();
+
+        for (int i = 0; i < C; i++){
+            String nome = leitor.next();
+            int N = leitor.nextInt();
+            if (nome.equals("Thor")) System.out.println("Y");
+            else if (! nome.equals("Thor") && N < 25000 ) System.out.println(("N"));
+        }
+    }
+}
