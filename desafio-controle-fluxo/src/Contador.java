c83b6b9e6ded8a2a5b1070859219ed5fb090bd75
Minni <lucasminni@hotmail.com>
2022-12-26 21:43:07 -0300

    desafio

--- desafio-controle-fluxo/src/Contador.java
+++ desafio-controle-fluxo/src/Contador.java
@@ -1,5 +1,31 @@
-import java.util.PrimitiveIterator;
-
+import java.util.Scanner;
 public class Contador {
- // https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/controle-fluxo
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        try{
+            contar(parametroUm, parametroDois);
+        }
+        catch (ParametrosInvalidosException e){
+            System.out.println("Erro! Segundo parâmetro maior que o primeiro.");
+        }
+
+    }
+
+static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+
+    if (parametroUm > parametroDois)
+        throw new ParametrosInvalidosException();
+    
+    int contagem = parametroDois - parametroUm;
+
+    for(int x=1; x <= contagem; x++){
+        System.out.println("Número " + x);
+    }
+    }
+
 }
